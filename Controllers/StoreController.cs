55cf2119e0c243a3710c85b9992e52ca8cb2ab49
June Nouh <june.nouh@outlook.com>
2022-06-24 22:42:43 +0200

    Add Store controller and index with details pages

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,41 @@
+﻿using BookStore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly BookStoreContext _context;
+        public StoreController(BookStoreContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Book.ToListAsync());
+        }
+
+        // GET: Books/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+    }
+}
